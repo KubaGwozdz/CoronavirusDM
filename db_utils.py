24087fdc2e8f9069348aa8db9221d219c231ffae
KubaGwozdz <jakub.gwozdz@icloud.com>
2020-03-15 21:36:49 +0100

    fast insert

--- db_utils.py
+++ db_utils.py
@@ -108,6 +108,75 @@ class DBManager:
             self.save_hashtags(tweet, hashtags)
             self.save_usermentions(tweet)
 
+    def fast_insert_user_mentions(self, tweets):
+        insert_sql = "INSERT OR IGNORE INTO user_mentions (tweet_id, user_id) VALUES (?, ?)"
+        vals = []
+        for tweet in tweets:
+            for um in tweet['user_mentions']:
+                vals.append((tweet['id'], um['id']))
+        self.cur.executemany(insert_sql, vals)
+        self.connection.commit()
+
+    def fast_insert_hashtags(self, tweets):
+        hashtag_tweets = dict()
+        for tweet in tweets:
+            for tag in tweet['hashtags']:
+                txt = tag['text']
+                if txt not in hashtag_tweets.keys():
+                    hashtag_tweets[txt] = []
+                hashtag_tweets[txt].append(tweet['id'])
+
+        insert_h_sql = "INSERT OR IGNORE INTO hashtag (text) VALUES (?)"
+        vals = list(map(lambda x: (x,)
+                        , hashtag_tweets.keys()))
+        self.cur.executemany(insert_h_sql, vals)
+        self.connection.commit()
+
+        h = list(hashtag_tweets.keys())
+        questionmarks = '?' * len(h)
+        select_h_sql = "SELECT id, text FROM hashtag WHERE text IN ({})".format(','.join(questionmarks))
+        self.cur.execute(select_h_sql, h)
+
+        h_id_tw_id = []
+        for tag in self.cur.fetchall():
+            for tweet_id in hashtag_tweets[tag['text']]:
+                h_id_tw_id.append((tag['id'], tweet_id))
+
+        insert_h_tw_sql = "INSERT OR IGNORE INTO hashtag_tweet (hashtag_id, tweet_id)  VALUES (?, ?)"
+        self.cur.executemany(insert_h_tw_sql, h_id_tw_id)
+        self.connection.commit()
+
+    def fast_insert_tweets(self, tweets):
+        insert_sql = "INSERT INTO tweet (id, user_id, text, lang, quote_count, reply_count, retweet_count, favorite_count, created_at, in_reply_to_status_id, in_reply_to_user_id, quoted_status_id)" \
+                     "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT(id) DO UPDATE SET " \
+                     "quote_count= MAX(excluded.quote_count, tweet.quote_count)," \
+                     "reply_count= MAX(excluded.reply_count, tweet.reply_count)," \
+                     "retweet_count= MAX(excluded.retweet_count, tweet.retweet_count)," \
+                     "favorite_count= MAX(excluded.favorite_count, tweet.favorite_count)"
+
+        vals = list(
+            map(
+                lambda tweet: (
+                    tweet['id'], tweet['user_id'], tweet['text'], tweet['lang'], tweet['quote_count'],
+                    tweet['reply_count'],
+                    tweet['retweet_count'], tweet['favorite_count'], tweet['created_at'],
+                    tweet['in_reply_to_status_id'],
+                    tweet['in_reply_to_user_id'], tweet['quoted_status_id'])
+                if 'quoted_status_id' in tweet else
+                (
+                    tweet['id'], tweet['user_id'], tweet['text'], tweet['lang'], tweet['quote_count'],
+                    tweet['reply_count'],
+                    tweet['retweet_count'], tweet['favorite_count'], tweet['created_at'],
+                    tweet['in_reply_to_status_id'],
+                    tweet['in_reply_to_user_id'], None)
+                , tweets
+            )
+        )
+        self.cur.executemany(insert_sql, vals)
+        self.connection.commit()
+        self.fast_insert_user_mentions(tweets)
+        self.fast_insert_hashtags(tweets)
+
     def update_user(self, new_user, old_user):
         names = []
         vals = []
@@ -127,7 +196,7 @@ class DBManager:
             self.connection.commit()
 
     def insert_user(self, user):
-        print(user)
+        # print(user)
         id = user['id']
 
         select_sql = "SELECT id, followers_count, friends_count FROM user WHERE id = ?"
@@ -145,8 +214,21 @@ class DBManager:
             self.cur.execute(insert_sql, val)
             self.connection.commit()
 
+    def fast_insert_users(self, users):
+        insert_sql = "INSERT INTO user (id, screen_name, location, followers_count, friends_count, created_at) " \
+                     "values (?, ?, ?, ?, ?, ?) ON CONFLICT(id) DO UPDATE SET " \
+                     "followers_count= MAX(excluded.followers_count, user.followers_count)," \
+                     "friends_count = MAX(excluded.friends_count, user.friends_count)"
+
+        vals = list(map(lambda user: (
+            user['id'], user['screen_name'], user['location'], user['followers_count'], user['friends_count'],
+            user['created_at']), users))
+
+        self.cur.executemany(insert_sql, vals)
+        self.connection.commit()
+
     def insert_retweets(self, retweets):
-        insert_sql = "INSERT INTO retweet" \
+        insert_sql = "INSERT OR IGNORE INTO retweet" \
                      " (id, user_id, tweet_id, created_at, reply_count, retweet_count, " \
                      "favorite_count, in_reply_to_status_id, in_reply_to_user_id) " \
                      "VALUES (?,?,?,?,?,?,?,?,?)"
--- json_sql_mapper.py
+++ json_sql_mapper.py
@@ -1,24 +1,22 @@
 import json
 import db_utils as db
-import file_parser
 
-rt_filenames = ['8_3_2020retweets.json']
+rt_filenames = ['13_3_2020retweets.json', '14_3_2020retweets.json']
 tw_data_dir = './data/tweets/'
-rt_data_dir = './data/retweets/'
+rtw_data_dir = './data/retweets/'
 usr_data_dir = './data/users/'
 
-data_dir = rt_data_dir
+data_dir = rtw_data_dir
 
 if __name__ == "__main__":
     db= db.DBManager()
     for filename in rt_filenames:
+        print(filename)
         with open(data_dir + filename) as f:
             data = json.load(f)
             if 'retweets' in data_dir:
                 db.insert_retweets(data)
             elif 'users' in data_dir:
-                for user in data:
-                    db.insert_user(user)
+                    db.fast_insert_users(data)
             else:
-                for tweet in data:
-                    db.insert_tweet(tweet)
+                db.fast_insert_tweets(data)
