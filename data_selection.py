28f7eabc49a00ef271a8d5cc1786e262ab71c9c4
KubaGwozdz <jakub.gwozdz@icloud.com>
2020-03-23 12:19:33 +0100

    hashtags per day

--- data_selection.py
+++ data_selection.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from db_utils import DBManager
 
 
@@ -7,7 +9,7 @@ class DataSelector(DBManager):
 
     def get_number_of_tweets_per_day(self):
         select_sql = "SELECT created_at , count(*) AS NUMBER FROM tweet " \
-                     "WHERE created_at > '2020-02-01' " \
+                     "WHERE created_at > '2020-03-07' " \
                      "GROUP BY DATE(created_at)"
         self.cur.execute(select_sql)
         data = self.cur.fetchall()
@@ -17,6 +19,30 @@ class DataSelector(DBManager):
             num = row[1]
             result[date] = num
         return result
+
+    def get_most_popular_hashtags_per_day(self):
+        select_sql = "SELECT hashtag.text, t.created_at, count(*) AS number from hashtag " \
+                     "JOIN hashtag_tweet h_t on hashtag.id = h_t.hashtag_id " \
+                     "JOIN tweet t on h_t.tweet_id = t.id " \
+                     "WHERE t.created_at > '2020-03-07' AND h_t.hashtag_id in (SELECT hashtag_id FROM hashtag_tweet " \
+                     "GROUP BY hashtag_id " \
+                     "ORDER BY count(*) DESC " \
+                     "LIMIT 10) " \
+                     "GROUP BY h_t.hashtag_id, date(t.created_at)"
+        self.cur.execute(select_sql)
+        data = self.cur.fetchall()
+        result = dict()
+        for row in data:
+            if row['text'] not in result:
+                result[row['text']] = dict()
+                result[row['text']]['dates'] = []
+                result[row['text']]['numbers'] = []
+
+            result[row['text']]['dates'].append(datetime.strptime(row['created_at'], "%Y-%m-%d %H:%M:%S").date())
+            result[row['text']]['numbers'].append(row['number'])
+
+        return result
+
 
     def get_most_popular_users(self):
         select_sql = "SELECT screen_name, followers_count, friends_count " \
--- simple_visualizations.py
+++ simple_visualizations.py
@@ -15,6 +15,22 @@ def tweets_per_day(db):
         y=Y,
         line_color='deepskyblue',
         opacity=0.8))
+    fig.update_layout(title_text="Tweets per day")
+    return fig
+
+
+def most_popular_hashtags_per_day(db):
+    data = db.get_most_popular_hashtags_per_day()
+    fig = go.Figure()
+    for tag in data.keys():
+        fig.add_trace(
+            go.Scatter(
+                x=data[tag]['dates'],
+                y=data[tag]['numbers'],
+                name=tag
+            )
+        )
+    fig.update_layout(title_text="Most popular hashtags per day")
     return fig
 
 def popular_users(db):
@@ -40,7 +56,11 @@ def popular_users(db):
 
 if __name__ == "__main__":
     db = DataSelector()
-    tweets_per_day_fig = tweets_per_day(db)
-    tweets_per_day_fig.show()
+    # tweets_per_day_fig = tweets_per_day(db)
+    # tweets_per_day_fig.show()
+
+    # tags_per_day_fig = most_popular_hashtags_per_day(db)
+    # tags_per_day_fig.show()
+
     popular_users_fig = popular_users(db)
     popular_users_fig.show()
